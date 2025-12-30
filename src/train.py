02deb48d2cbf1534af4129a4962b191c2ad93e35
sumanth1975 <b.sumanth2006@gmail.com>
2025-12-30 21:57:15 +0530

    Delete src directory

--- src/train.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-
-def train(X,y):
-    vectorizer = TfidfVectorizer(stop_words="english")
-    Xv = vectorizer.fit_transform(X)
-    model = LogisticRegression()
-    model.fit(Xv,y)
-    return model, vectorizer
